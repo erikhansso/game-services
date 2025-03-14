2172983fe0d7b54e361078458f94054c2934d8f5
Erik Hansson <erik.hansson@protonmail.com>
2025-03-14 13:15:39 +0100

    fix: build and app tests

--- game-session/src/test/kotlin/com/dharmit/game_services/game_session/GameSessionApplicationTests.kt
+++ game-session/src/test/kotlin/com/dharmit/game_services/game_session/GameSessionApplicationTests.kt
@@ -2,8 +2,10 @@ package com.dharmit.game_services.game_session
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GameSessionApplicationTests {
 
 	@Test
--- matchmaking/src/test/kotlin/com/dharmit/game_services/matchmaking/MatchMakingApplicationTests.kt
+++ matchmaking/src/test/kotlin/com/dharmit/game_services/matchmaking/MatchMakingApplicationTests.kt
@@ -2,8 +2,10 @@ package com.dharmit.game_services.matchmaking
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MatchMakingApplicationTests {
 
 	@Test
